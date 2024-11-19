4df87737da93485a6a4d2b9b2479c124c0f0e80d
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-19 10:44:26 +0800

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,9 @@
+from colorama import Fore, Style
+
+banner = f"""
+{Fore.YELLOW}╔════════════════════════════════════════╗
+{Fore.YELLOW}║      🚀 Meshchain Ai 自动工具 🚀       ║
+{Fore.YELLOW}║  👤    脚本编写：@qklxsqf              ║
+{Fore.YELLOW}║  📢  电报频道：https://t.me/ksqxszq    ║
+{Fore.YELLOW}╚════════════════════════════════════════╝{Style.RESET_ALL}
+"""
